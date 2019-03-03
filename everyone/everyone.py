541ab857f0e7668d9535bafb13f435a56d86632b
Adam Lavin <adam@socrates.systems>
2019-03-03 17:53:31 +0000

    Update checks

--- everyone/everyone.py
+++ everyone/everyone.py
@@ -1,9 +1,13 @@
-from redbot.core import commands
+from redbot.core import commands, checks
 
 
 class Everyone(commands.Cog):
 
     @commands.command(hidden=True, aliases=["ss"])
+    @checks.bot_in_a_guild()
+    @checks.guildowner()
+    @checks.admin()
+    @checks.mod()
     async def silentsay(self, ctx, *, msg: str):
         """Say things as the bot and deletes the command if it can"""
         if ctx.channel.permissions_for(ctx.guild).manage_messages:
